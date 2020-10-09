024c010916967e5f6ae3b1793f767e85a507ccf2
xramos17 <58446715+xramos17@users.noreply.github.com>
2020-10-09 16:23:22 +0100

    Added Time & date & Icon

--- src/app.js
+++ src/app.js
@@ -1,3 +1,49 @@
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let daynum = date.getDate();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  return `${day},${daynum} ${month} `;
+}
+
+function formatHour(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getminutes();
+  if (minutes < 10) {
+    minutes = `0${minutes} `;
+  }
+
+  return `${hours}:${minutes} `;
+}
+
 function displayCurrentTemp(response) {
   document.querySelector("#current-temp").innerHTML = Math.round(
     response.data.main.temp
@@ -10,8 +56,14 @@ function displayCurrentTemp(response) {
     response.data.wind.speed
   );
   document.querySelector("#w-country").innerHTML = response.data.sys.country;
+  document.querySelector("#date-hours").innerHTML = formatHour(
+    response.data.dt * 1000
+  );
+  document.querySelector("#date-city").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
-
+let city = "Lisbon";
 let apiKey = "1adcec3e50018a8b64c974c018ae3653";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
